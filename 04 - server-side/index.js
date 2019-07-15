b63dbd1c7d6af004a372cf44e1e7ba40abd8e8bc
Facu Saldaa <nachher@hotmail.com>
2019-07-14 22:41:44 -0300

    kind a working , stablishing my own server

--- 04 - server-side/index.js
+++ 04 - server-side/index.js
@@ -2,3 +2,16 @@ const express = require('express');
 const app = express();
 app.listen(3000, () => console.log("listening at 3000"));
 app.use(express.static('public'));
+app.use(express.json({limit: '1mb'}));
+
+
+app.post('/api', (request, response) => {
+    console.log('Tengo un request');
+    console.log(request.body);
+    const data  = request.body;
+    response.json({
+        status: 'success',
+        latitude : data.lat,
+        longitude : data.lon 
+    });
+});

fd0b4467de23bb4dc4020d8f9bfa43e49529c347
Facu Saldaa <nachher@hotmail.com>
2019-07-16 00:54:48 -0300

    fixed

--- 04 - server-side/index.js
+++ 04 - server-side/index.js
@@ -1,17 +1,22 @@
 const express = require('express');
+const Datastore = require('nedb');
 const app = express();
 app.listen(3000, () => console.log("listening at 3000"));
 app.use(express.static('public'));
 app.use(express.json({limit: '1mb'}));
 
+const database = new Datastore('database.db');
+database.loadDatabase();
+
 
 app.post('/api', (request, response) => {
-    console.log('Tengo un request');
     console.log(request.body);
     const data  = request.body;
+    database.insert(data);
+    console.log(database);
     response.json({
         status: 'success',
-        latitude : data.lat,
-        longitude : data.lon 
+        latitude : data.lat ,
+        longitude : data.lon
     });
 });

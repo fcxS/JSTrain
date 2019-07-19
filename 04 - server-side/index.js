56b8e328971e22b175c2e5cc4bb0b13ad7035343
Facu Saldaa <nachher@hotmail.com>
2019-07-18 22:53:03 -0300

    finished other day works

--- 04 - server-side/index.js
+++ 04 - server-side/index.js
@@ -3,20 +3,29 @@ const Datastore = require('nedb');
 const app = express();
 app.listen(3000, () => console.log("listening at 3000"));
 app.use(express.static('public'));
-app.use(express.json({limit: '1mb'}));
+app.use(express.json({
+    limit: '1mb'
+}));
 
 const database = new Datastore('database.db');
 database.loadDatabase();
 
 
+app.get('/api', (request, response) => {
+    database.find({}, (err, data) => {
+        if (err) {
+            response.end();
+            return;
+        }
+        response.json(data);
+    })
+});
+
 app.post('/api', (request, response) => {
-    console.log(request.body);
-    const data  = request.body;
+    console.log("Tengo una request");
+    const data = request.body;
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
     database.insert(data);
-    console.log(database);
-    response.json({
-        status: 'success',
-        latitude : data.lat ,
-        longitude : data.lon
-    });
+    response.json(data);
 });
